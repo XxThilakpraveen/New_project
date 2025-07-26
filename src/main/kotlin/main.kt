2319dea3fba652b31cc40c3c1cdf48a875052d4e
ThilakP34aveen <100140846+XxThilakpraveen@users.noreply.github.com>
2025-07-26 10:07:14 +0530

    random array exercise 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,34 @@
 import kotlin.random.Random
 
 fun main() {
-    val new_Arr = Array(25) {it +1 }
+
+
+    print("Enter Array size: ")
+    val arraySize = readLine()?.toIntOrNull() ?: 0
+
+    println("Enter Array Elements")
+    val user_arr = Array<Int>(arraySize) { readLine()?.toIntOrNull() ?: 0 }
+
     val numbers = MutableList(50)   { it + 1 }
-    println(numbers.size)
 
     for (i in numbers.indices) {
-        numbers[i] = Random.nextInt(1, 1000)
+        numbers[i] = Random.nextInt(1, 20)
     }
-
+    user_arr.sort()
+    numbers.sort()
     println(numbers.joinToString())
-    for (i in 0..24) {
-        val rand_indx = Random.nextInt(numbers.size)
-        println(rand_indx)
-        numbers.removeAt(rand_indx)
+
+    for ( value in user_arr ) {
+        if (value <= numbers[numbers.size - 1])
+            if (numbers.contains(value) ){
+
+                val index = numbers.indexOf(value)
+                println("the removed index is $index and the value is $value")
+                numbers[index] = 0
+            }
+
     }
-
-    println(new_Arr.joinToString())
     println(numbers.joinToString())
-    println(numbers.size)
+    val No_zero_array = numbers.filter { it != 0 }.toTypedArray()
+    println(No_zero_array.joinToString())
 }
-
